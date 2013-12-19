d2acf40236deeabef8bcecb03bffc442ad915cca
emag <lanabe.lanabe@gmail.com>
2013-12-19 11:17:40 +0900

    Add simple plain text version

--- src/main/java/org/study/java/jboss/javaee5/session/SimpleAccessCounterServlet.java
+++ src/main/java/org/study/java/jboss/javaee5/session/SimpleAccessCounterServlet.java
@@ -1,56 +1,36 @@
 package org.study.java.jboss.javaee5.session;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.logging.Logger;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Logger;
 
 /**
  * Servlet implementation class AccessCounterServlet
  */
-public class AccessCounterServlet extends HttpServlet {
+public class SimpleAccessCounterServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    private static Logger logger = Logger.getLogger(AccessCounterServlet.class.getName());
-
-    private static final String TITLE = "Access Counter";
-
-    private static final String HEADER =
-                    "<html>" +
-                        "<head>" +
-                        "<title>" +
-                            TITLE +
-                        "</title>" +
-                        "</head>" +
-                    "<body>" +
-                    "<h1>" + TITLE + "</h1>";
-
-    private static final String FOOTER =
-                    "</body>" +
-                    "</html>";
-
+    private static Logger logger = Logger.getLogger(SimpleAccessCounterServlet.class.getName());
 
     /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     * @see javax.servlet.http.HttpServlet#doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doPost(request, response);
     }
 
     /**
-     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     * @see javax.servlet.http.HttpServlet#doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        response.setContentType("text/html; charset=UTF-8");
+        response.setContentType("text/plain; charset=UTF-8");
         PrintWriter out = response.getWriter();
-
-        out.println(HEADER);
 
         HttpSession session = request.getSession(true);
 
@@ -65,9 +45,6 @@ public class AccessCounterServlet extends HttpServlet {
         session.setAttribute("yourCounter", counter);
 
         out.println("Session ID:" + session.getId() + ", " + counter + " Times Access.");
-        out.println("<a href=\"" + response.encodeURL("accesscounter") + "\">reload</a>");
-
-        out.println(FOOTER);
 
         out.close();
 
